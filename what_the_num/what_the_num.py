44642198f15369e851f78ed234fa78a4935432e9
Alex Ntavelos <a.ntavelos@gmail.com>
2018-02-21 09:22:43 +0100

    implement numbersapi

--- what_the_num/what_the_num.py
+++ what_the_num/what_the_num.py
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import requests
+
+URL = "http://numbersapi.com/{number}/{num_type}?json"
+
+
+def fetch(number, num_type):
+    try:
+        response = requests.get(URL.format(number=number, num_type=num_type))
+    except Exception:
+        raise Exception("Something went wrong. Try again.")
+
+    if response.status_code == 404:
+        raise Exception("Invalid data provided.")
+
+    if response.status_code in [400, 500]:
+        raise Exception("Something went wrong. Please try again.")
+
+    return response.json()
+
+
+def parse(response):
+    # single item response
+    if "text" in response:
+        return {response['number']: response.get('text')}
+    # multiple item responses
+    else:
+        return {int(key): response[key]['text'] for key in response}
+
+
+def what_the_num(number, num_type):
+    return parse(fetch(number, num_type))
